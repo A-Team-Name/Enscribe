ac43c378bb8af1d3576d666fa1f3638e4f199bf8
Aidan Hall <aidan.hall202@gmail.com>
2024-10-11 18:13:28 +0100

    Character Drawing UI
    This includes code for canvas drawing and an interface for quickly
    generating training data for a given character set.

--- backend/static/canvas_drawing.js
+++ backend/static/canvas_drawing.js
@@ -1,3 +1,95 @@
-function setupCanvasDrawing(canvas, controls) {
+// Adapted from:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+const setupCanvasDrawing = (canvas, controls) => {
     let ctx = canvas.getContext("2d");
+
+    ctx.lineWidth = controls.lineWidth.value || 3;
+
+    const clearCanvas = () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    controls.lineWidth.addEventListener(
+	"input",
+	(event) => {
+	    ctx.lineWidth = event.target.value;
+	    for (const label of event.target.labels) {
+		label.innerHTML = ctx.lineWidth + "px";
+	    }
+	}
+    );
+
+    controls.clear.addEventListener("click", clearCanvas);
+    controls.save.addEventListener("click",
+				   (event) => {
+				       // Clear the canvas, delayed until after save.
+				       setTimeout(clearCanvas, 200);
+				       // Remove the link text, so we don't accidentally save over
+				       // a drawing by clicking multiple times.
+				       controls.save.innerHTML = "";
+				   })
+
+    controls.randomSymbol.addEventListener(
+	"click",
+	(event) => {
+	    // Clear the canvas.
+	    clearCanvas();
+	    let symbols = controls.symbolSet.value;
+	    let chosen_index = getRandomInt(0, symbols.length);
+	    console.log(chosen_index);
+	    let chosen_symbol = symbols.substring(chosen_index, chosen_index + 1);
+	    console.log(chosen_symbol);
+	    controls.writtenSymbol.value = chosen_symbol;
+	}
+    )
+
+    const generateSaveLink = (event) => {
+	// Set up the save link so clicking it downloads the currently drawn character.
+	controls.save.href = canvas.toDataURL();
+	controls.save.download =
+	    "u"
+	    + controls.writtenSymbol.value.codePointAt(0).toString(16)
+	    + "-" + Date.now() + ".png";
+	controls.save.innerHTML = "Save: " + controls.save.download;
+    }
+
+    const drawLine = (event) => {
+	if (event.buttons & 1) {
+	    ctx.lineTo(event.offsetX, event.offsetY);
+	    ctx.stroke();
+	    drawPoint(event);
+	    generateSaveLink();
+	}
+    }
+
+    const drawPoint = (event) => {
+	const circle = new Path2D();
+	// Radius must be half ctx.lineWidth so diameter matches lines.
+	circle.arc(event.offsetX, event.offsetY, ctx.lineWidth / 2, 0, 2 * Math.PI);
+	ctx.fill(circle);
+    }
+
+    const penDown = (event) => {
+	ctx.beginPath();
+	ctx.moveTo(event.offsetX, event.offsetY);
+    }
+
+    canvas.addEventListener("mousedown", drawPoint);
+    canvas.addEventListener("mousedown", penDown);
+    canvas.addEventListener("mousedown", generateSaveLink);
+
+    canvas.addEventListener("mouseenter", penDown);
+
+    canvas.addEventListener("mouseup", drawLine);
+
+    canvas.addEventListener("mouseleave", drawLine);
+
+    canvas.addEventListener("mousemove", drawLine);
 }
