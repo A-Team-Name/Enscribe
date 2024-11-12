09c9d48383f9926fea2d25663afb6c71e9570eec
raven0034 <monkeyboy2805@gmail.com>
2024-11-12 17:36:26 +0000

    Fix minor issue on collectstatic where it complains when expecting input

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-poetry run python ./manage.py collectstatic
+poetry run python ./manage.py collectstatic --no-input
 poetry run python ./manage.py migrate
 if [ "$DJANGO_ENV" = "development" ]; then
     poetry run python ./manage.py runserver 0.0.0.0:"${PORT:-5000}"
