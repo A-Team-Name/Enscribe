5f2a5fd5c393d34f354aa3dd6bf28a1b4b83c01a
Aidan Hall <aidan.hall202@gmail.com>
2024-11-13 16:13:59 +0000

    Added HTML and CSS files for main page (/code)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('index/', views.index),
-    path('draw/', views.draw)
+    path("", views.index),
+    path("index/", views.index),
+    path("draw/", views.draw),
+    path("code/", views.code),
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -3,10 +3,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-
 # Create your views here.
 def index(request: WSGIRequest) -> HttpResponse:
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def draw(request: WSGIRequest) -> HttpResponse:
-    return render(request, 'canvas_drawing.html')
+    return render(request, "canvas_drawing.html")
+
+
+def code(request: WSGIRequest) -> HttpResponse:
+    return render(request, "code.html")
