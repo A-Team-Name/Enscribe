2c5369ebb3063e2e948c3c5e15d0d824ad85832b
Aidan Hall <aidan.hall@outlook.com>
2024-11-20 09:59:57 +0000

    whiteboard.mjs: Factored out Whiteboard component
    Web components are a good model for this kind of interaction.

--- backend/static/main_app.js
+++ backend/static/main_app.js
@@ -1,36 +1,4 @@
-const whiteboard = {
-    container: document.getElementById("whiteboard"),
-    background: document.getElementById("whiteboard-background"),
-    code: document.getElementById("whiteboard-code"),
-    annotations: document.getElementById("whiteboard-annotations"),
-    ui: document.getElementById("whiteboard-ui"),
-    input: document.getElementById("whiteboard-input"),
-
-    get width() {
-        return this.background.width;
-    },
-    get height() {
-        return this.background.height;
-    },
-
-    /* Set the width of the whiteboard to value (an integer number of pixels). */
-    set width(value) {
-        this.background.width = value;
-        this.code.width = value;
-        this.annotations.width = value;
-        this.container.style["width"] = value + "px";
-    },
-
-    /* Set the height of the whiteboard to value (an integer number of pixels). */
-    set height(value) {
-        this.background.height = value;
-        this.code.height = value;
-        this.annotations.height = value;
-        this.container.style["height"] = value + "px";
-    },
-};
-
-whiteboard.width = whiteboard.height = 5000;
+import { Whiteboard } from '/static/modules/whiteboard.mjs';
 
 const settings = {
     openButton: document.getElementById("open-settings"),
