2762719e718a929a33ad06ad11b1b23910d2b163
Joshua Fitzmaurice <jfitz02@outlook.com>
2024-11-22 13:14:11 +0000

    updated entrypoint to not always build public_static and fixed post endpoint in index.html

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-poetry run python ./manage.py collectstatic --no-input
-poetry run python ./manage.py migrate
 if [ "$DJANGO_ENV" = "development" ]; then
     poetry run python ./manage.py runserver 0.0.0.0:"${PORT:-5000}"
 else
