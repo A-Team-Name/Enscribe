75a51e00ff5f95ae2f8481dd7b239a1c506ed262
los <lewis.solarin@bbconsult.co.uk>
2025-01-15 16:07:41 +0000

    implement login functionality with superuser

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("", views.index),
     path("index/", views.index),
     path("draw/", views.draw),
-    path("app/", views.main_app),
-    path("layering/", views.layering_demo),
     path("execute/", views.execute),
     path("image_to_text/", views.image_to_text),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
