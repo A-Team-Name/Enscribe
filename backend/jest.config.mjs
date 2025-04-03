042a5491156ba6716b4bcdfb534d166b8166084f
Aidan Hall <aidan.hall@warwick.ac.uk>
2025-04-03 15:27:36 +0100

    Jest: jsdom testing environment
    This allows us to test modules that manipulate the DOM.

--- backend/jest.config.mjs
+++ backend/jest.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
-    testMatch: [ "**/__tests__/**/*.?(m)[jt]s?(x)", "**/?(*.)+(spec|test).?(m)[jt]s?(x)" ]
+    testMatch: [ "**/__tests__/**/*.?(m)[jt]s?(x)", "**/?(*.)+(spec|test).?(m)[jt]s?(x)" ],
+    testEnvironment: "jsdom",
 };
 
 export default config;
