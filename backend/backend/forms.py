1e9e4d0aedf3b6dfc80eb0095a09b3fa72440941
jfitz02 <jfitz02@outlook.com>
2025-04-11 14:35:46 +0100

    docstrings+docs

--- backend/backend/forms.py
+++ backend/backend/forms.py
@@ -4,6 +4,20 @@ from .models import Image
 
 
 class ImageForm(forms.ModelForm):
+    """Form for uploading an image.
+
+    Inherits:
+        forms.ModelForm: Django model form class
+    """
+
     class Meta:
+        """
+        Meta class for the ImageForm.
+
+        Attributes:
+            model (Image): The model to use for the form.
+            fields (list): The fields to include in the form.
+        """
+
         model = Image
         fields = ["name", "img"]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,18 @@ from django.urls import path
 
 from . import views
 
+"""
+backend URL Configuration
+The `urlpatterns` list routes URLs to views.
+ - "/": The main page of the app
+    - "/index/": The main page of the app
+    - "/execute/": The API endpoint for executing code
+    - "/image_to_text/": The API endpoint for converting images to text
+    
+"""
 urlpatterns = [
     path("", views.index),
     path("index/", views.index),
-    path("draw/", views.draw),
     path("execute/", views.execute),
     path("image_to_text/", views.image_to_text),
-    path("lambda_calculus/", views.get_random_lambda_line),
 ]
